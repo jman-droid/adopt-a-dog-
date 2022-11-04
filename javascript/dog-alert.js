8ad7358c4e0f4a0e15dc656fbe1593a5db4e7e8f
code personn (beginner level) <63759439+jman-droid@users.noreply.github.com>
2022-11-04 12:59:29 -0700

    added functionality to dog images and buttons

---
+++ javascript/dog-alert.js
@@ -0,0 +1,11 @@
+let fee = 123.45;
+let total = 0;
+
+function totalAdder(fee) {
+
+  total+=fee;
+
+  alert(total);
+
+
+}
---
+++ javascript/dog-info.js
@@ -0,0 +1,15 @@
+function showInfo(name, breed,fee) {
+  alert(' Name:'+ name + ' Breed:'+ breed + ' Fee: $'+ fee);
+}
+
+let fee = 123.45;
+let total = 0;
+
+function totalAdder(fee) {
+
+  total = fee + total;
+
+  alert(total);
+
+
+}
