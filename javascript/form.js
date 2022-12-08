74d69f796c3c58505c8a7d5fba4aa83d29a1c8f7
code personn (beginner level) <63759439+jman-droid@users.noreply.github.com>
2022-12-08 14:46:22 -0800

    jquery! Yaya

--- javascript/form.js
+++ javascript/form.js
@@ -1,5 +1,10 @@
 const checkoutWrapper = document.querySelector('.checkout-wrapper');
 
+
+const h1 = document.createElement('h1');
+h1.textContent = 'Reach out if you have any questions!';
+checkoutWrapper.appendChild(h1);
+
 const fields =  [
   {
     name: 'full-name',
@@ -18,6 +23,7 @@ const fields =  [
 ];
 
 const form = document.createElement('form');
+form.id = "contact-us";
 
 checkoutWrapper.appendChild(form);
 
@@ -29,7 +35,9 @@ for (let i= 0; i < fields.length; i+= 1){
     label.setAttribute('for', field.name);
 
     const input = document.createElement('input');
-    input.setAttribute('id', field.name);
+    input.setAttribute('name', field.name);
+    input.setAttribute('type', 'text');
+
 
     form.appendChild(label);
     form.appendChild(input);
@@ -43,15 +51,22 @@ submitButton.textContent = 'Submit';
 
 form.appendChild(submitButton);
 
-form.addEventListener('submit', function(event) {
+// form.addEventListener('submit', function(event) {
+//   event.preventDefault();
+//
+//   for (let i= 0; i < fields.length; i+= 1){
+//     const field = fields[i];
+//     const element = document.getElementById(field.name);
+//     console.log(`${field.label}: ${element.value}`);
+//   }
+//
+//   alert('Thank you. The form information has been received.');
+//
+// });
+
+
+$( "#contact-us" ).on("submit", function(event){
   event.preventDefault();
-
-  for (let i= 0; i < fields.length; i+= 1){
-    const field = fields[i];
-    const element = document.getElementById(field.name);
-    console.log(`${field.label}: ${element.value}`);
-  }
-
-  alert('Thank you. The form information has been received.');
-
-});
+  var arr = $(this).serializeArray();
+  console.log(arr);
+  })
